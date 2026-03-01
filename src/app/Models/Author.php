abb8648623af95f44085c8da53fa9d530136dca3
Mateusz <mcmatipl6460@gmail.com>
2026-03-01 17:40:55 +0100

    Add Book and Author models with migrations and factories

---
+++ src/app/Models/Author.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Author extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'last_book_title',
+    ];
+
+    public function books(): BelongsToMany
+    {
+        return $this->belongsToMany(Book::class)
+                    ->withTimestamps();
+    }
+}
---
+++ src/app/Models/Book.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Book extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    public function authors(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class)
+                    ->withTimestamps();
+    }
+}
